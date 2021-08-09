e4966b6d45441f70549b86fa439a7273e1d6087a
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2021-08-10 02:31:48 +0300

    Fixed bug

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,22 +1,25 @@
 jQuery( document ).ready(function( $ ) {
 	"use strict";
+	setInterval(function () {
 		$('.owl-carousel').owlCarousel({
-		    items:4,
-		    lazyLoad:true,
-		    loop:true,
-		    dots:true,
-		    margin:20,
-		    responsiveClass:true,
-			    responsive:{
-			        0:{
-			            items:1,
-			        },
-			        600:{
-			            items:2,
-			        },
-			        1000:{
-			            items:4,
-			        }
-			    }
+			items:4,
+			lazyLoad:true,
+			loop:true,
+			dots:true,
+			margin:20,
+			responsiveClass:true,
+			responsive:{
+				0:{
+					items:1,
+				},
+				600:{
+					items:2,
+				},
+				1000:{
+					items:4,
+				}
+			}
 		});
+	},500);
+
 });
