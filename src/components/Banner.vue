12cd9afce4fb8bda74a19b8098fc57149655ce0a
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2021-08-10 01:37:26 +0300

    Updated banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,12 +4,15 @@
       <div class="row">
         <div class="col-md-12">
           <div class="caption">
-            <h2>Ecommerce HTML Template</h2>
+            <h2>Open Source eCommerce Project</h2>
             <div class="line-dec"></div>
-            <p>Pixie HTML Template can be converted into your desired CMS theme. Total <strong>5 pages</strong> included. You can use this Bootstrap v4.1.3 layout for any CMS.
-              <br><br>Please tell your friends about <a rel="nofollow" href="https://www.facebook.com/tooplate/">Tooplate</a> free template site. Thank you. Photo credit goes to <a rel="nofollow" href="https://www.pexels.com">Pexels website</a>.</p>
+            <p>Spring shop is open source eCommerce project. It developed by using <strong>Spring Boot</strong>, <strong>VueCli</strong> and <strong>Postgresql.</strong> It uses client server and layered architecture.
+              <br><br>
+              You can reach the backend code at <a target="_blank" href="https://github.com/cagatayy94/spring-shop">here.</a>
+              <br>You can reach the frontend code at <a target="_blank" href="https://github.com/cagatayy94/spring-shop">here.</a>
+            </p>
             <div class="main-button">
-              <a href="#">Order Now!</a>
+              <a target="_blank" href="">Source Code</a>
             </div>
           </div>
         </div>
