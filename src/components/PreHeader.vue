b565d1b55ea05e32623f931cd49b350e3f7b93af
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2021-08-10 00:40:10 +0300

    Implemented theme

---
+++ src/components/PreHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="pre-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <span>Suspendisse laoreet magna vel diam lobortis imperdiet</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PreHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
